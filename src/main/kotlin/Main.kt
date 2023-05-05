6446dbc2ba494db322c69c2f3671ec859ebca432
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-05-05 23:59:07 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main(){
+ naming("janet",18,"kenya")
+ println("akirachix")
+ name("maria")
+
+}
+
+
+
+
+
+
+
+
+ //Write a function that takes in 3 parameters, name, age, and country, and returns a String with this structure “Hi, my name is x,
+ // I am y years old and I am from z.” Where x, y, and z are the provided name, age, and country respectively.
+// (3 points)
+fun naming(name:String,age:Int,country:String){
+println("I am $name I am $age years old and I am from $country")
+}
+
+// Write a function that takes in a String and returns its length (2 points)
+fun numArr(str: String):Int{
+ return str.length
+
+
+}
+
+
+// Write a function that takes in a name and prints out “That’s me!” when your name is passed to it, otherwise,
+// it prints out “I don’t know who that is” (3 points)
+fun name(name: String){
+ var y="janet"
+ if (name==y){
+  println("Thats me")
+ }
+ else{
+  println("I dont know who that is")
+ }
+
+}
+
+
+
+
+
+
+
+// Write a Kotlin function that takes in a string will all the vowels replaces by the character ‘*’.
+ // Use string interpolation to generate the output. (2 points)
+fun vowels(){
+ var a=("Beautiful")
+
+}
